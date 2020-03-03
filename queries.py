48621c430841f2ad0d57c51863deb5169cc41519
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-03-03 12:27:41 -0800

    Added view_notes_by_name method (updated method to now check first and last name

--- queries.py
+++ queries.py
@@ -1,12 +1,12 @@
-def view_notes_by_name(patient_name):
+def view_notes_by_name(patient_firstname, patient_lastname):
     get_patient_notes = ''' SELECT N.content AS Content, N.date AS Date, P.purpose AS Purpose, M.LastName AS Author
         FROM Note N
             JOIN MedicalStaff M ON(M.ID = N.AuthorID)
             JOIN Appointment A ON(A.ID = N.AppointmentId)
             JOIN Purpose P ON(P.Id = A.purposeId)
             JOIN Patient PA ON(PA.id = A.patientId)
-        WHERE PA.name LIKE '{}'
-    '''.format(patient_name)
+        WHERE PA.FirstName LIKE '{}' AND PA.LastName LIKE '{}' 
+    '''.format(patient_firstname, patient_lastname)
     return get_patient_notes
 
 
