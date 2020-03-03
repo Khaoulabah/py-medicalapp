6eadcb2b4bed83ddff0f4032202783da5c8e9bdc
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-03-03 12:21:28 -0800

    Added view_notes_by_name method

--- queries.py
+++ queries.py
@@ -1,3 +1,15 @@
+def view_notes_by_name(patient_name):
+    get_patient_notes = ''' SELECT N.content AS Content, N.date AS Date, P.purpose AS Purpose, M.LastName AS Author
+        FROM Note N
+            JOIN MedicalStaff M ON(M.ID = N.AuthorID)
+            JOIN Appointment A ON(A.ID = N.AppointmentId)
+            JOIN Purpose P ON(P.Id = A.purposeId)
+            JOIN Patient PA ON(PA.id = A.patientId)
+        WHERE PA.name LIKE '{}'
+    '''.format(patient_name)
+    return get_patient_notes
+
+
 GET_EMPLOYEES = '''SELECT * FROM Employees'''
 GET_PATIENT_CONTACTINFO = '''   SELECT FirstName, LastName, Number as PhoneNumber,
                                     Name as PhoneType, StreetAddress, AppNumber, City, State, ZipCode
